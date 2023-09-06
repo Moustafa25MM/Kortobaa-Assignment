cd39899b4a01c7b6d4e5888bde0a18f585bafde1
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-09-06 22:03:41 +0300

    User Schema

---
+++ src/models/user.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../util/database';
+
+interface UserAttributes {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+}
+
+class User extends Model<UserAttributes> implements UserAttributes {
+  public id!: number;
+  public username!: string;
+  public password!: string;
+  public email!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+  }
+);
+
+export default User;
