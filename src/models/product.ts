5227ee47abde909bbc706296fab7c6fc63061c08
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-09-06 23:04:57 +0300

    Establish association between products and users

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,11 +1,13 @@
 import { DataTypes, Model } from 'sequelize';
 import { sequelize } from '../util/database';
+import User from './user';
 
 interface ProductAttributes {
   id: number;
   title: string;
   image: string;
   price: number;
+  userId: number;
 }
 
 class Product extends Model<ProductAttributes> implements ProductAttributes {
@@ -13,6 +15,7 @@ class Product extends Model<ProductAttributes> implements ProductAttributes {
   public title!: string;
   public image!: string;
   public price!: number;
+  public userId!: number;
 }
 
 Product.init(
@@ -34,11 +37,22 @@ Product.init(
       type: DataTypes.FLOAT,
       allowNull: false,
     },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
   },
   {
     sequelize,
     modelName: 'Product',
   }
 );
+
+Product.belongsTo(User, { foreignKey: 'userId' });
 
 export default Product;
